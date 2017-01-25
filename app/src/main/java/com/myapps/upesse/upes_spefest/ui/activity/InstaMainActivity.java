fa833e17d66c0a70de0630bb0137382026d68906
Aniruddha Tapas <aniruddhastapas@gmail.com>
2017-01-25 23:57:55 +0530

    Update InstaMainActivity.java

--- app/src/main/java/com/myapps/upesse/upes_spefest/ui/activity/InstaMainActivity.java
+++ app/src/main/java/com/myapps/upesse/upes_spefest/ui/activity/InstaMainActivity.java
@@ -1,20 +1,26 @@
-package com.myapps.upesse.upes_spefest.ui.activity;
+package com.myapps.upessefest2017.ui.activity;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,16 +29,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.OnClick;
+//import butterknife.BindView;
+//import butterknife.OnClick;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -41,14 +52,26 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
-import com.myapps.upesse.upes_spefest.R;
-import com.myapps.upesse.upes_spefest.auth.MainActivity;
-import com.myapps.upesse.upes_spefest.ui.utils.Utils;
-import com.myapps.upesse.upes_spefest.ui.view.FeedContextMenu;
-import com.myapps.upesse.upes_spefest.ui.view.FeedContextMenuManager;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.myapps.upessefest2017.R;
+import com.myapps.upessefest2017.auth.MainActivity;
+import com.myapps.upessefest2017.ui.utils.Utils;
+import com.myapps.upessefest2017.ui.view.FeedContextMenu;
+import com.myapps.upessefest2017.ui.view.FeedContextMenuManager;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+
+import com.myapps.upessefest2017.ui.activity.BasicImageDownloader;
+import com.myapps.upessefest2017.ui.activity.BasicImageDownloader.ImageError;
+import com.myapps.upessefest2017.ui.activity.BasicImageDownloader.OnImageLoaderListener;
+
 
 import static com.firebase.ui.auth.ui.ExtraConstants.EXTRA_IDP_RESPONSE;
 
@@ -65,11 +88,18 @@ public class InstaMainActivity extends BaseDrawerActivity implements PostsFragme
 
 //    @BindView(R.id.rvFeed)
 //    RecyclerView rvFeed;
+    /*
     @BindView(R.id.btnCreate)
     FloatingActionButton fabCreate;
     @BindView(R.id.content)
     CoordinatorLayout clContent;
+    */
+    FloatingActionButton fabCreate;
+    CoordinatorLayout clContent;
     ImageButton btnMore;
+
+    boolean storage;
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
 
     //private FeedAdapter feedAdapter;
     private MenuItem inboxMenuItem;
@@ -82,7 +112,16 @@ public class InstaMainActivity extends BaseDrawerActivity implements PostsFragme
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instamain);
+        fabCreate = (FloatingActionButton)findViewById(R.id.btnCreate);
+        fabCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onTakePhotoClick();
+            }
+        });
+        clContent= (CoordinatorLayout)findViewById(R.id.content);
         my_recycler_view = (RecyclerView)findViewById(R.id.my_recycler_view);
+
         //btnMore = (ImageButton)findViewById(R.id.btnMore);
         //btnMore.setOnClickListener(this);
         setupFeed();
@@ -115,31 +154,6 @@ public class InstaMainActivity extends BaseDrawerActivity implements PostsFragme
         tabLayout.setupWithViewPager(viewPager);
 
     }
-
-/*
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        if (ACTION_SHOW_LOADING_ITEM.equals(intent.getAction())) {
-            showFeedLoadingItemDelayed();
-        }
-    }
-
-
-
-
-    private void showFeedLoadingItemDelayed() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                my_recycler_view.smoothScrollToPosition(0);
-            }
-        }, 500);
-    }
-
-  */
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -229,8 +243,150 @@ public class InstaMainActivity extends BaseDrawerActivity implements PostsFragme
 
     @Override
     public void onSharePhotoClick(int feedItem) {
-        FeedContextMenuManager.getInstance().hideContextMenu();
+        //FeedContextMenuManager.getInstance().hideContextMenu();
+        //Toast.makeText(InstaMainActivity.this, Integer.toString(feedItem), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(InstaMainActivity.this, FeedContextMenuManager.getCurrentImgUrl(), Toast.LENGTH_SHORT).show();
+        boolean result = checkPermission(InstaMainActivity.this);
+        storage = result;
+
+        String imgURL = FeedContextMenuManager.getCurrentImgUrl();
+        String fileName = "SAVED_IMG-"+ new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date()) + ".jpg";
+
+        if(storage){
+            downloadImageFromUri(imgURL,fileName);
+        }
+        else{
+            Toast.makeText(this,"External storage permission is necessary",Toast.LENGTH_LONG).show();
+        }
+
+        /*
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference httpsReference = storage.getReferenceFromUrl(FeedContextMenuManager.getCurrentImgUrl());
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        //Path to /data/data/UPES-SPE-Fest-2017/app_data/Saved_Images
+        File dir = cw.getDir("Saved_Images", Context.MODE_PRIVATE);
+        //Create Saved_Images dir
+        String fileName = "SAVED_IMG-"+ new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date()) + ".jpg";
+        final File imgPath = new File(dir,fileName);
+        httpsReference.getFile(imgPath).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                // Local temp file has been created
+                    Toast.makeText(getApplicationContext(),"Image is saved at " + imgPath, Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                Toast.makeText(getApplicationContext(),"Sorry, Image not saved!", Toast.LENGTH_LONG).show();
+            }
+        });
+        */
     }
+
+    private void downloadImageFromUri(String imgURL, final String fileName) {
+        final TextView tvPercent = (TextView) findViewById(R.id.tvPercent);
+        final ProgressBar pbLoading = (ProgressBar) findViewById(R.id.pbImageLoading);
+        pbLoading.setVisibility(View.VISIBLE);
+        tvPercent.setVisibility(View.VISIBLE);
+        pbLoading.bringToFront();
+        tvPercent.bringToFront();
+
+        final BasicImageDownloader downloader = new BasicImageDownloader(new BasicImageDownloader.OnImageLoaderListener() {
+            @Override
+            public void onError(ImageError error) {
+                Toast.makeText(InstaMainActivity.this, "Error code " + error.getErrorCode() + ": " +
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                error.printStackTrace();
+                tvPercent.setVisibility(View.GONE);
+                pbLoading.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onProgressChange(int percent) {
+                pbLoading.setProgress(percent);
+                tvPercent.setText(percent + "%");
+            }
+
+            @Override
+            public void onComplete(Bitmap result) {
+                        /* save the image - I'm gonna use JPEG */
+                final Bitmap.CompressFormat mFormat = Bitmap.CompressFormat.JPEG;
+                        /* don't forget to include the extension into the file name */
+                final File myImageFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        File.separator + "UPES-SPE-Fest-Saved-Images" + File.separator + fileName + "." + mFormat.name().toLowerCase());
+                BasicImageDownloader.writeToDisk(myImageFile, result, new BasicImageDownloader.OnBitmapSaveListener() {
+                    @Override
+                    public void onBitmapSaved() {
+                        Toast.makeText(InstaMainActivity.this, "Image saved as: " + myImageFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onBitmapSaveError(ImageError error) {
+                        Toast.makeText(InstaMainActivity.this, "Error code " + error.getErrorCode() + ": " +
+                                error.getMessage(), Toast.LENGTH_LONG).show();
+                        error.printStackTrace();
+                    }
+
+
+                }, mFormat, false);
+
+                tvPercent.setVisibility(View.GONE);
+                pbLoading.setVisibility(View.GONE);
+            }
+        });
+        downloader.download(imgURL, true);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    public static boolean checkPermission(final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((android.app.Activity) context, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    android.support.v7.app.AlertDialog.Builder alertBuilder = new android.support.v7.app.AlertDialog.Builder(context);
+                    alertBuilder.setCancelable(true);
+                    alertBuilder.setTitle("Permission necessary");
+                    alertBuilder.setMessage("External storage permission is necessary");
+                    alertBuilder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions((android.app.Activity) context, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                        }
+                    });
+                    android.support.v7.app.AlertDialog alert = alertBuilder.create();
+                    alert.show();
+
+                } else {
+                    ActivityCompat.requestPermissions((Activity) context, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    storage = true;
+                } else {
+                    //code for deny
+                }
+                break;
+        }
+    }
+
 
     @Override
     public void onCopyShareUrlClick(int feedItem) {
@@ -243,7 +399,6 @@ public class InstaMainActivity extends BaseDrawerActivity implements PostsFragme
     }
 
 
-    @OnClick(R.id.btnCreate)
     public void onTakePhotoClick() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null || user.isAnonymous()) {
@@ -296,6 +451,13 @@ public class InstaMainActivity extends BaseDrawerActivity implements PostsFragme
             }
         });
     }
+
+    @Override
+    public void onMoreClick(View v, int position, String thumb_url) {
+        FeedContextMenuManager.getInstance().toggleContextMenuFromView(v, position, this);
+        FeedContextMenuManager.setCurrentImgUrl(thumb_url);
+    }
+
 
     private void signOut() {
         AuthUI.getInstance()
